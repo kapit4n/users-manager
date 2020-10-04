5a2b5007bc038835b898be399d647de4ccc30bab
Luis Arce <luis.arce22@gmail.com>
2020-10-04 16:58:50 -0400

    validate user_id input

--- server/routes/users.service.js
+++ server/routes/users.service.js
@@ -3,6 +3,19 @@ var express = require('express');
 var router = express.Router();
 var HttpStatus = require('http-status-codes');
 
+
+router.param('user_id', function (req, res, next, user_id) {
+  console.log('Performing validations on ' + user_id);
+
+  // We can decode the input and update
+  req.user_id = decodeURI(user_id).trim();
+
+  console.log('validated ' + user_id);
+
+  // Allow the request to continue
+  next();
+})
+
 // Your enpoints will be configured here
 router.get('/', function (req, res) {
   res.status(HttpStatus.OK).send('Request received');
@@ -12,6 +25,13 @@ router.get('/', function (req, res) {
 router.get('/:user_id', function (req, res) {
   var user_id = req.params.user_id;
   res.status(HttpStatus.OK).send('Request received for ' + user_id)
+})
+
+// Your endpoints will be configured here
+router.delete('/', function (req, res) {
+  var user_id = req.body.user_id;
+
+  res.status(HttpStatus.ACCEPTED).send(user_id)
 })
 
 // Makes these configurations available when imported
